ebc874abf13061bd778ba458e8c2c06cea794d48
Diego Suque <suquediegocosta@gmail.com>
2026-05-20 11:55:28 -0300

    Configura deploy no GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/portfolio-diego-suque",
+  assetPrefix: "/portfolio-diego-suque/",
+};
 
 export default nextConfig;
